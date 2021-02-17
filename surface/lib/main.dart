47f429d5ce9d4f6fd62ab93eace012567e3ae164
sowJamng <maodosow96@gmail.com>
2021-02-17 21:37:47 +0000

    feat: home Page

---
+++ surface/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'Screens/Home.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final String titre='International Well Control Form Surface B';
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: titre,
+      theme: ThemeData( 
+        backgroundColor: Colors.black,
+       
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
